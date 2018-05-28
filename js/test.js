3666ccba55278dfb2abf56c0362954ab93afee4e
Evan Dorsky <evan.dorsky@icloud.com>
2018-05-28 00:09:59 -0700

    Slightly nicer primitive APIs

--- js/test.js
+++ js/test.js
@@ -1,24 +1,34 @@
 $(document).ready(() => {
     const app = new PIXI.Application()
-
     document.body.appendChild(app.view)
 
-    let line_ctx = new PIXI.Graphics()
-    line_ctx.lineStyle(4, 0xFFFFFF, 1)
-    line_ctx.moveTo(0, 0)
-    line_ctx.lineTo(80, 50)
-    line_ctx.x = 32
-    line_ctx.y = 32
+    function drawLine(x, y, dx, dy) {
+        let line = new PIXI.Graphics()
+        line.x = x
+        line.y = y
 
-    line_ctx.arc(100, 100, 30, 0, Math.PI);
-    app.stage.addChild(line_ctx)
+        line.lineStyle(4, 0xffffff, 1)
 
-    var circles = [] 
-    let circle_ctx = new PIXI.Graphics()
-    circle_ctx.beginFill(0x9966FF)
-    circle_ctx.drawCircle(0, 0, 32)
-    circle_ctx.endFill()
-    app.stage.addChild(circle_ctx)
-    circle_ctx.x = 500
-    circle_ctx.y = 50
+        line.moveTo(0, 0)
+        line.lineTo(dx, dy)
+
+        app.stage.addChild(line)
+        return line
+    }
+
+    function drawCircle(x, y, r) {
+        let circle = new PIXI.Graphics()
+        circle.x = x
+        circle.y = y
+
+        circle.beginFill(0xffffff)
+        circle.drawCircle(0, 0, r)
+        circle.endFill()
+
+        app.stage.addChild(circle)
+        return circle
+    }
+
+    let line = drawLine(100, 100, 50, 50)
+    let circle = drawCircle(50, 50, 50)
 })
