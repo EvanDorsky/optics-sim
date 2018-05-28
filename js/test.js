a111ffe0eaacd4333dcc18dcef44033b7257ad1f
Evan Dorsky <evan.dorsky@icloud.com>
2018-05-27 23:54:43 -0700

    More learning

--- js/test.js
+++ js/test.js
@@ -1,24 +1,24 @@
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
 $(document).ready(() => {
-    const app = new PIXI.Application();
+    const app = new PIXI.Application()
 
-    document.body.appendChild(app.view);
+    document.body.appendChild(app.view)
 
-    let line = new PIXI.Graphics();
-    line.lineStyle(4, 0xFFFFFF, 1);
-    line.moveTo(0, 0);
-    line.lineTo(80, 50);
-    line.x = 32;
-    line.y = 32;
-    app.stage.addChild(line);
+    let line_ctx = new PIXI.Graphics()
+    line_ctx.lineStyle(4, 0xFFFFFF, 1)
+    line_ctx.moveTo(0, 0)
+    line_ctx.lineTo(80, 50)
+    line_ctx.x = 32
+    line_ctx.y = 32
 
-    let circle = new PIXI.Graphics();
-    circle.beginFill(0x9966FF);
-    circle.drawCircle(0, 0, 32);
-    circle.endFill();
-    circle.x = 64;
-    circle.y = 130;
-    app.stage.addChild(circle);
+    line_ctx.arc(100, 100, 30, 0, Math.PI);
+    app.stage.addChild(line_ctx)
+
+    var circles = [] 
+    let circle_ctx = new PIXI.Graphics()
+    circle_ctx.beginFill(0x9966FF)
+    circle_ctx.drawCircle(0, 0, 32)
+    circle_ctx.endFill()
+    app.stage.addChild(circle_ctx)
+    circle_ctx.x = 500
+    circle_ctx.y = 50
 })
